cff0ff4d7b1bdb4ae2d4b6e8a33f7698bb58e0c1
Mancuel <mancumason@gmail.com>
2025-05-09 15:56:22 +0200

    add Central

---
+++ app/EC_Central.py
@@ -0,0 +1,31 @@
+import json
+
+# APLICACION CENTRAL
+
+# Modulo Validacion de Taxis
+
+#Modulo Conexion de Cliente
+
+def clientConexion(ip):
+    consumer = KafkaConsumer(
+            'clientes',
+            bootstrap_servers= ip,
+            auto_offset_reset='latest',
+            enable_auto_commit=True,
+            group_id='clientes'
+        )
+
+    for message in consumer:
+        
+        pass
+
+#Modulo Carga de Localizaciones
+def localizationCharge():
+    with open('localizations.json', 'r') as file:
+        localizations = json.load(file)
+
+
+
+# Main
+if __name__ == "__main__":
+    localizationCharge()
